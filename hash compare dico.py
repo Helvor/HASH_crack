d868f42d00c8349e15512c74b2e6399173b90dcf
Elveli <50722149+Helvor@users.noreply.github.com>
2022-12-31 12:41:40 +0100

    update with latin-1

--- hash compare dico.py
+++ hash compare dico.py
@@ -3,12 +3,12 @@ import hashlib
 passHash = input("Hash (sha3-512) : ")
 wordlist_hash = ''
 name_dico = input("Name of the dico file : ")
-file = open(name_dico, 'r')
+file = open(name_dico, 'r', encoding='latin-1')
 
 while wordlist_hash != passHash:
         for line in file:
                 password = line.strip('\n')
-                wordlist_hash = hashlib.sha3_512(password.encode('utf-8')).hexdigest()
+                wordlist_hash = hashlib.sha3_512(password.encode('latin-1')).hexdigest()
                 print(f"Password : {password} - Hash : {wordlist_hash}")
                 if wordlist_hash == passHash:
                         print(f'Password : {password}')
